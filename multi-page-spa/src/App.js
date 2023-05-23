068fd4fbead56679540fddf5a51b469254ded10c
Mr-Hera <randellhera@gmail.com>
2023-05-24 00:19:28 +0300

    Feat: Completed Single Page App basics

---
+++ multi-page-spa/src/App.js
@@ -0,0 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail';
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<Home />} />
+//     <Route path='/products' element={<ProductsPage />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'products', element: <ProductsPage /> },
+      { path: 'products/:productId', element: <ProductDetailPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
